a92ee3fcc94feb16a61daa9c455a7da571741df1
aaronbush3 <aaronbush3@gmail.com>
2023-06-08 09:47:44 -0400

    Structure and DB setup

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import Button from '@/components/ui/button'
+
+interface PageProps {
+  
+}
+
+const page: FC<PageProps> = ({}) => {
+    return <Button>Hello</Button>
+}
+
+export default page
