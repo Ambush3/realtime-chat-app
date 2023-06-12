c261ba52378961c621b78bc6b8c044ad2f56f3c0
aaronbush3 <aaronbush3@gmail.com>
2023-06-11 22:14:00 -0400

    Add friend request in side bar.

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -4,6 +4,9 @@ import { notFound } from 'next/navigation'
 import React, { FC, ReactNode } from 'react'
 import Link from 'next/link'
 import { Icon, Icons } from '@/components/Icons'
+import Image from 'next/image'
+import SignOutButton from '@/components/SignOutButton'
+import FriendRequestsSidebarOption from '@/components/FriendRequestsSidebarOption'
 
 interface LayoutProps {
     children: ReactNode
@@ -73,6 +76,34 @@ const Layout = async ({ children } : LayoutProps)=> {
                                     )
                                 })}
                             </ul>
+                        </li>
+
+                        <li>
+                            <FriendRequestsSidebarOption />
+                        </li>
+                        
+
+                        <li className='-mx-6 mt-auto flex items-center'>
+                            <div className='flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900'>
+                                <div className='relative h-8 w-8 bg-gray-50'>
+                                    <Image 
+                                        fill
+                                        referrerPolicy='no-referrer'
+                                        className='rounded-full'
+                                        src={session.user.image || ''}
+                                        alt='Your profile picture'
+                                    />
+                                </div>
+                                
+                                <span className='sr-only'>Your profile</span>
+                                <div className='flex flex-col'>
+                                    <span aria-hidden='true'>{session.user.name}</span>
+                                    <span className='text-xs text-zinc-400' aria-hidden='true'>
+                                        {session.user.email}
+                                    </span>
+                                </div>
+                            </div>
+                            <SignOutButton className='h-full aspect-square'/>
                         </li>
                     </ul>
                 </nav>
