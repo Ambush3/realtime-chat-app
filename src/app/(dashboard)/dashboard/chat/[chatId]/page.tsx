c8cfa1bc5a5b381d1b59081b51aa4ca81dd18c7d
aaronbush3 <aaronbush3@gmail.com>
2023-06-19 20:13:54 -0400

    Fix return statement and add Image for each participant

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -1,10 +1,12 @@
 import { fetchRedis } from '@/helpers/redis'
 import { authOptions } from '@/lib/auth'
 import { db } from '@/lib/db'
-import { messageArrayValidator } from '@/lib/validations/messages'
+import { messageArrayValidator } from '@/lib/validations/message'
 import { getServerSession } from 'next-auth'
 import { notFound } from 'next/navigation'
 import Image from 'next/image'
+import Messages from '@/components/Messages'
+import ChatInput from '@/components/ChatInput'
 
 interface PageProps {
   params: {
@@ -40,7 +42,6 @@ const page = async ({params}: PageProps ) => {
 
   const { user } = session
 
-  // access to chat id 
   const [userId1, userId2] = chatId.split('--')
 
   if (user.id !== userId1 && user.id !== userId2) {
@@ -51,31 +52,36 @@ const page = async ({params}: PageProps ) => {
   const chatPartner = (await db.get(`user:${chatPartnerId}`)) as User
   const initialMessages = await getChatMessages(chatId)
 
-  return <div className='flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)]'>
-    <div className='flex sm:items-center justify-between py-3 border-b-2 border-gray-200'>
-      <div className='relative flex items-center space-x-4'>
-        <div className='relatve'>
-          <div className='ms-3 relative w-8 sm:w-12 h-8 sm:h-12'>
-              <Image
-                fill
-                referrerPolicy='no-referrer'
-                src={chatPartner.image}
-                alt={`${chatPartner.name} profile picture`}
-                className='rounded-full'
-              />
-            </div>
-        </div>
-        <div className='flex flex-col leading-tight'>
-          <div className='text-xl flex items-center'>
-              <span className='text-gray-700 mr-3 font-semibold'>
-                {chatPartner.name}
-              </span>
+  return (
+    <div className='flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)]'>
+      <div className='flex sm:items-center justify-between py-3 border-b-2 border-gray-200'>
+        <div className='relative flex items-center space-x-4'>
+          <div className='relatve'>
+            <div className='ms-3 relative w-8 sm:w-12 h-8 sm:h-12'>
+                <Image
+                  fill
+                  referrerPolicy='no-referrer'
+                  src={chatPartner.image}
+                  alt={`${chatPartner.name} profile picture`}
+                  className='rounded-full'
+                />
+              </div>
           </div>
-          <span className='text-sm text-gray-600'>{chatPartner.email}</span>
+          <div className='flex flex-col leading-tight'>
+            <div className='text-xl flex items-center'>
+                <span className='text-gray-700 mr-3 font-semibold'>
+                  {chatPartner.name}
+                </span>
+            </div>
+            <span className='text-sm text-gray-600'>{chatPartner.email}</span>
+          </div>
         </div>
       </div>
+
+      <Messages chatPartner={chatPartner} sessionImg={session.user.image} sessionId={session.user.id} initialMessages={initialMessages}/>
+      <ChatInput chatId={chatId} chatPartner={chatPartner}/>
     </div>
-  </div>
+  )
 }
 
 export default page
