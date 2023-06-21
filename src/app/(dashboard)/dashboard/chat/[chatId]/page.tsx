725eb272b4abc6b456693fb2f68a6212526c4846
aaronbush3 <aaronbush3@gmail.com>
2023-06-20 20:45:18 -0400

    Add real time features to chat application. Toast popup, message counter, and real time messages in chatbox.

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -78,7 +78,7 @@ const page = async ({params}: PageProps ) => {
         </div>
       </div>
 
-      <Messages chatPartner={chatPartner} sessionImg={session.user.image} sessionId={session.user.id} initialMessages={initialMessages}/>
+      <Messages chatId={chatId} chatPartner={chatPartner} sessionImg={session.user.image} sessionId={session.user.id} initialMessages={initialMessages}/>
       <ChatInput chatId={chatId} chatPartner={chatPartner}/>
     </div>
   )
--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -1,18 +1,67 @@
 'use client'
 
-import { chatHrefConstructor } from '@/lib/utils'
+import { pusherClient } from '@/lib/pusher'
+import { chatHrefConstructor, toPusherKey } from '@/lib/utils'
 import { usePathname, useRouter } from 'next/navigation'
 import { FC, useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
+import UnseenChatToast from '@/components/UnseenChatToast'
 
 interface SidebarChatlistProps {
     friends: User[],
     sessionId: string
 }
 
+interface ExtendedMessage extends Message {
+    senderId: string,
+    senderImg: string
+    senderName: string
+}
+
 const SidebarChatlist: FC<SidebarChatlistProps> = ({friends, sessionId}) => {
     const router = useRouter()
     const pathname = usePathname()
     const [unseenMessages, setUnseenMessages] = useState<Message[]>([])
+
+    useEffect(() => {
+        pusherClient.subscribe(toPusherKey(`user:${sessionId}:chats`))
+        pusherClient.subscribe(toPusherKey(`user:${sessionId}:friends`))
+        
+        const newFriendHandler = () => {
+            router.refresh()
+        }
+
+        const chatHandler = (message: ExtendedMessage) => {
+            const shouldNotify = pathname !== `/dashboard/chat/${chatHrefConstructor(sessionId, message.senderId)}`
+
+            if(!shouldNotify) return
+
+            // should be notified
+            toast.custom((t) => (
+                <UnseenChatToast 
+                    t={t}
+                    sessionId={sessionId}
+                    senderId={message.senderId}
+                    senderImg={message.senderImg}
+                    senderName={message.senderName}
+                    senderMessage={message.text}
+                />
+            ))
+
+            setUnseenMessages((prev) => {
+                return [...prev, message]
+            })
+
+        }
+
+        pusherClient.bind('new_message', chatHandler)
+        pusherClient.bind('new_friend', newFriendHandler)
+
+        return () => {
+            pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:chats`))
+            pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:friends`))
+        }
+    }, [pathname, sessionId, router])
 
     useEffect(() => {
         if (pathname?.includes('chat')) {
