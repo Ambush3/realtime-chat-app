23d0c8211129786e0d53d1fb4f4f4c5e03811798
aaronbush3 <aaronbush3@gmail.com>
2023-06-23 10:22:57 -0400

    Fixing overload match issue to db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,10 @@
 import { Redis } from '@upstash/redis'
 
-export const db = new Redis({
-    url: process.env.UPSTASH_REDIS_REST_URL,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN,
-})
+const redisUrl = process.env.UPSTASH_REDIS_REST_URL;
+const redisToken = process.env.UPSTASH_REDIS_REST_TOKEN;
+
+if (!redisUrl || !redisToken) {
+    throw new Error('Redis URL or token not provided.');
+}
+
+export const db = new Redis({ url: redisUrl, token: redisToken });
