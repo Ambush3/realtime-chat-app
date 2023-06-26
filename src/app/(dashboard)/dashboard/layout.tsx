b77f02ae53cd52264b76bfa248feed170bcce2fb
aaronbush3 <aaronbush3@gmail.com>
2023-06-26 10:34:21 -0400

    Change width of sidebar.

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -57,7 +57,7 @@ const Layout = async ({ children }: LayoutProps) => {
                 />
             </div>
 
-            <div className='hidden md:flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6'>
+            <div className='hidden md:flex h-full w-full max-w-[380px] grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6'>
                 <Link href='/dashboard' className='flex h-16 shrink-0 items-center'>
                     <Icons.Logo className='h-8 w-auto text-indigo-600' />
                 </Link>
