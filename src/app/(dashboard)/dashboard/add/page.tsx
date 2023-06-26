4908ea0d211ce91a512c4c79e93b2947811c37c4
aaronbush3 <aaronbush3@gmail.com>
2023-06-26 16:28:23 -0400

    UI changes, capitalization, corrections.

--- src/app/(dashboard)/dashboard/add/page.tsx
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -4,7 +4,7 @@ import { FC } from 'react'
 const page: FC = () => {
   return (
     <main className='pt-8'>
-      <h1 className='font-bold text-5xl mb-8'>Add a friend</h1>
+      <h1 className='font-bold text-5xl mb-8'>Add A Friend</h1>
       <AddFriendButton />
     </main>
   )
