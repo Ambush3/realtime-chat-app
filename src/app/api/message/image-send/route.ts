f9a4766218f48586a2ac484b70dfe2d05a99eac1
aaronbush3 <aaronbush3@gmail.com>
2024-02-09 08:15:07 -0500

    Change cloudinary config to only take in CLOUDINARY_URL

--- src/app/api/message/image-send/route.ts
+++ src/app/api/message/image-send/route.ts
@@ -1,17 +1,9 @@
-import multer from 'multer';
 import Cloudinary from 'cloudinary';
-
-console.log('we are in the route file')
 
 // Configure Cloudinary
 Cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET,
+    CLOUDINARY_URL: process.env.CLOUDINARY_URL,
 });
-
-console.log(process.env.CLOUDINARY_CLOUD_NAME, process.env.CLOUDINARY_API_KEY, process.env.CLOUDINARY_SECRET,)
-
 
 // API endpoint for image upload
 export default async function handler(req: any, res: any) {
@@ -22,7 +14,7 @@ export default async function handler(req: any, res: any) {
         res.status(200).json({ url: secure_url });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Something went wrong in the route' });
+        res.status(500).json({ error: 'Something went wrong during image upload' });
     }
 }
 
