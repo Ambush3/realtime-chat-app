8618a8c403beda1fabba844dbb33ba955071b830
aaronbush3 <aaronbush3@gmail.com>
2024-02-18 19:56:38 -0500

    FIx for handler function error

--- src/app/api/message/image-send/route.ts
+++ src/app/api/message/image-send/route.ts
@@ -1,3 +1,4 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import Cloudinary from 'cloudinary';
 
 // Configure Cloudinary
@@ -5,10 +6,13 @@ Cloudinary.v2.config({
     CLOUDINARY_URL: process.env.CLOUDINARY_URL,
 });
 
-// API endpoint for image upload
-export default async function handler(req: any, res: any) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ error: 'Method Not Allowed' });
+    }
+
     try {
-        const { file } = req.files;
+        const { file } = req.body;
         const { secure_url } = await Cloudinary.v2.uploader.upload(file.path);
 
         res.status(200).json({ url: secure_url });
@@ -17,6 +21,3 @@ export default async function handler(req: any, res: any) {
         res.status(500).json({ error: 'Something went wrong during image upload' });
     }
 }
-
-
-
