9b680696d89d05f0781cb5b0694bfc73bc37a054
aaronbush3 <aaronbush3@gmail.com>
2024-03-01 22:31:52 -0500

    Add a new router ts file specifically for NextJs 13 post routing for images to cloudinary

---
+++ src/app/api/message/image-send/[...post].ts
@@ -0,0 +1,25 @@
+// src/app/api/message/image-send/[...post].ts
+
+import { NextApiRequest, NextApiResponse } from 'next';
+import Cloudinary from 'cloudinary';
+
+Cloudinary.v2.config({
+    CLOUDINARY_URL: process.env.CLOUDINARY_URL,
+});
+
+
+export default async function imageSendHandler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ error: 'Method Not Allowed' });
+    }
+
+    try {
+        const { file } = req.body;
+        const { secure_url } = await Cloudinary.v2.uploader.upload(file.path);
+
+        res.status(200).json({ url: secure_url });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Something went wrong during image upload' });
+    }
+}
