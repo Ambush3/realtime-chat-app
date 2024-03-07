16a2e738fff68a3a0fcd1b77c59971cedd487aed
aaronbush3 <aaronbush3@gmail.com>
2024-03-06 20:07:15 -0500

    Remove code for image-send route for now
    fix for conflict

---
+++ src/app/api/message/image-send/route.ts
@@ -0,0 +1,22 @@
+import Cloudinary from 'cloudinary';
+
+// // Configure Cloudinary
+// Cloudinary.v2.config({
+//     CLOUDINARY_URL: process.env.CLOUDINARY_URL,
+// });
+
+// // API endpoint for image upload
+// export default async function handler(req: any, res: any) {
+//     try {
+//         const { file } = req.files;
+//         const { secure_url } = await Cloudinary.v2.uploader.upload(file.path);
+
+//         res.status(200).json({ url: secure_url });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: 'Something went wrong during image upload' });
+//     }
+// }
+
+
+
