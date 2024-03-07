8e0d4c04ad90bc92c2273be30cbd43fe81a04b69
aaronbush3 <aaronbush3@gmail.com>
2024-03-06 20:23:54 -0500

    Remove file button text/button from chat area. Remove image-send route till I fix out handler export
    FIX PLEEEEASE

---
+++ src/components/FileUploadButton.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+interface FileUploadButtonProps {
+    onFileChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    onClear: () => void;
+    selectedFile: File | null;
+}
+
+const FileUploadButton: FC<FileUploadButtonProps> = ({ onFileChange, onClear, selectedFile }) => {
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            onFileChange(event);
+        }
+    };
+
+    const removeFile = () => {
+        onClear();
+    };
+
+    return (
+        <div>
+            <label htmlFor="file-upload" className="text-blue-500 cursor-pointer">
+            </label>
+            <input id="file-upload" type="file" className="hidden" onChange={handleFileChange} />
+            {selectedFile && <div>Selected File: {selectedFile.name}</div>}
+            {selectedFile && (
+                <button className="text-red-500 hover:text-red-600 cursor-pointer" onClick={removeFile}>
+                    Remove
+                </button>
+            )}
+        </div>
+    );
+};
+
+export default FileUploadButton;
